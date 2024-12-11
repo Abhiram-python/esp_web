d2918cb4c6ff0268b782c5ee66f67154fbf69ef2
Abhiram-python <gurujalaabhiram@gmail.com>
2024-12-11 12:46:26 +0530

    fourth

--- esp-web.py
+++ esp-web.py
@@ -2,27 +2,35 @@ from flask import *
 
 app = Flask(__name__)
 
+new_mess=False
+
 @app.route('/',methods=["GET"])
 def home():
     return render_template("home.html")
 
 @app.route('/esp_timer',methods=["POST"])
 def esp_timer():
-    global mess
+    global mess,new_mess
     data = request.json
     mess=data["minutes"]
+
+    new_mess=True
 
     return "lekhya"
 
 @app.route('/timer_data',methods=["POST","GET"])
 def timer_data():
-    try:
-        return mess
-    except:
-        return "hello"
+    global new_mess
+    if new_mess==True:
+        new_mess=False
+        return [mess,"n"]
+    else:
+        return "1"
     
 @app.route('/esp_stop',methods=["POST"])
 def esp_stop():
     global mess
     data=request.json
     mess=data["mess"]
+
+    return "rith"
